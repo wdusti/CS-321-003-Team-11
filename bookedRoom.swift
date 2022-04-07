375cd02b4eec767c08349d7f35d6a343d3babf9a
wdusti <94583923+wdusti@users.noreply.github.com>
2022-04-07 18:45:47 -0400

    Update bookedRoom.swift

--- bookedRoom.swift
+++ bookedRoom.swift
@@ -15,3 +15,20 @@ private let JSONdata = try Data(contentsOf: path)
 //Creates built in decoder and decodes the data into the protocol defined by openModel
 private let decoder = JSONDecoder()
 private let data = try decoder.decode([bookedModel].self, from: JSONdata)
+
+//Function to return the user's booked rooms
+//str = the uname of the user
+func getBooked(str: String) -> [bookedModel] {
+    //return value, array of bookedModel
+    var ret: [bookedModel] = []
+    
+    //loop to append each bookedModel
+    for i in data {
+        //checks for uname match
+        if(i.uname == str) {
+            ret.append(i)
+        }
+    }
+    
+    return ret
+}
