627ba067b70f5b4c5ea230c81da94297777374ec
wdusti <94583923+wdusti@users.noreply.github.com>
2022-04-07 15:51:03 -0400

    Update openRoom.swift

--- openRoom.swift
+++ openRoom.swift
@@ -3,18 +3,18 @@ import SwiftUI
 struct openModel: Decodable {
     let date: Int
     let roomNumber: String
-    let slots: [Int]
+    var slots: [Int]
 }
 
 //Finds the file in the document directory
-let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("openData.json")
+private let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("openData.json")
 
 //Gets the contents of the file as Data
-let JSONdata = try Data(contentsOf: path)
+private let JSONdata = try Data(contentsOf: path)
 
 //Creates built in decoder and decodes the data into the protocol defined by openModel
-let decoder = JSONDecoder()
-let data = try decoder.decode([openModel].self, from: JSONdata)
+private let decoder = JSONDecoder()
+private let data = try decoder.decode([openModel].self, from: JSONdata)
 
 //Function to get rooms open on the given date and startTime
 func getOpen(date: Int, startTime: Int) -> [openModel] {
@@ -24,12 +24,11 @@ func getOpen(date: Int, startTime: Int) -> [openModel] {
     
     //loop appending possible bookings
     for i in data {
-        //currently only adds rooms with the given date
-        if(i.date == date) {
+        //adds the openModel if the date has the given startTime
+        if(i.date == date && i.slots.contains(startTime)) {
             ret.append(i)
         }
     }
     
-    //returns all possible start times on the given date for the room
     return ret
 }
