4b8625f561dcbb3f81eec1ccb14a59bf4b5c6483
wdusti <94583923+wdusti@users.noreply.github.com>
2022-04-11 12:04:52 -0400

    Update bookedRoom.swift

--- bookedRoom.swift
+++ bookedRoom.swift
@@ -7,28 +7,36 @@ struct bookedModel: Decodable {
     var slot: [Int]
 }
 
-private let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("bookedData.json")
+private let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+private let path = directory!.appendingPathComponent("bookedData.json")
 
-//Gets the contents of the file as Data
-private let JSONdata = try Data(contentsOf: path)
-
-//Creates built in decoder and decodes the data into the protocol defined by openModel
+//Creates built in decoder and encoder
 private let decoder = JSONDecoder()
-private let data = try decoder.decode([bookedModel].self, from: JSONdata)
+private let encoder = JSONEncoder()
 
 //Function to return the user's booked rooms
 //str = the uname of the user
 func getBooked(str: String) -> [bookedModel] {
-    //return value, array of bookedModel
-    var ret: [bookedModel] = []
+    //read data from JSON
+    let decoder = JSONDecoder()
+    let data = try decoder.decode([bookedModel].self, from: JSONdata)
     
-    //loop to append each bookedModel
-    for i in data {
-        //checks for uname match
-        if(i.uname == str) {
-            ret.append(i)
-        }
-    }
+    //filters bookedModel with matching uname(username)
+    let ret = data.filter {$0.uname == uname}
     
     return ret
+}
+
+//function to book a time slot
+func addtoBooked(date: Int, roomNumber: String, uname: String, slot: [Int]) -> Void {
+    //read contents of JSON file
+    let JSONdata = try Data(contentsOf: path)
+    var data = try decoder.decode([bookedModel].self, from: JSONdata)
+    
+    //adds the booking to the array of JSON objects
+    data.append(bookedModel(date: date, roomNumber: roomNumber, uname: uname, slot: slot))
+    
+    //writes array of JSON objects to file
+    let str = try encoder.encode(data)
+    try str.write(to: path)
 }
