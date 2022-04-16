16b041fad7f2922947d5a2d708bc1f2c13ae5158
nalluri7 <100387594+nalluri7@users.noreply.github.com>
2022-04-15 22:07:28 -0400

    Update connectToFirebase.swift

--- emailAuthentication/connectToFirebase.swift
+++ emailAuthentication/connectToFirebase.swift
@@ -2,20 +2,12 @@
 // Connects to Firebase when app starts.
 // Code from Firebase documentation.
 
-import UIKit
 import Firebase
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, 
+func application(_ application: UIApplication, 
                     didFinishLaunchingWithOptions launchOptions:
                     [UIApplicationLaunchOptionsKey: Any]?) -> {
 
         FirebaseApp.configure()
         return true
     }
-    
-}
